84b00da3fe0ca56730477459a6b96a47ec3e53d5
Meng Fei Wong <mengfei.wong@swisslog.com>
2025-01-04 10:49:10 +0100

    Added Asynchronous Features
    Added Asynchronous Features in order to run multithreading

---
+++ src/main/java/com/analyzer/datahandler/messenger/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.analyzer.datahandler.messenger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfig {
+
+    @Bean
+    public TaskExecutor initTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("testmqtt-");
+        executor.initialize();
+        return executor;
+    }
+}
+
