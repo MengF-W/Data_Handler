9ce218bb0c851e89e689d9beccd3efbf712d177b
MengF-W <123597866+MengF-W@users.noreply.github.com>
2025-04-15 16:40:02 +0200

    Added the get all data webservice end point
     The get all data webservice end point is added in order to retrieve all data from database

--- src/main/java/com/analyzer/datahandler/database/controller/DatabaseController.java
+++ src/main/java/com/analyzer/datahandler/database/controller/DatabaseController.java
@@ -9,7 +9,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/db")
 public class DatabaseController {
 
@@ -27,6 +31,14 @@ public class DatabaseController {
         Device device = JsonParser.getInstance().deserializeJson(content,Device.class);
         deviceRepository.save(device);
         return new ResponseEntity<>(RESPONSE_CREATED_MESSAGE, HttpStatus.CREATED);
+
+    }
+
+    @GetMapping(path= "/getAllData" ,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Device> getAllData() {
+        List<Device> devices = deviceRepository.findAll();
+        return devices.stream().sorted((p1, p2) -> (p1.getTimeStamp()).compareTo(p2.getTimeStamp())).collect(Collectors.toList());
 
     }
 
